24ceb5eabe171e624577eb033dd06675a18b2ef6
Paul Lewis <paulxlewis@me.com>
2021-01-05 21:34:20 +0000

    project simple logic

--- iftest/condition03.py
+++ iftest/condition03.py
@@ -2,15 +2,15 @@
 hostname = input("What value should we set for hostname?")
 ## Notice how the next line has changed
 ## here we use the str.lower() method to return a lowercase string
-if hostname.lower() == "mtg":
+if hostname.lower().strip() == "mtg":
     print("The hostname was found to be mtg")
     print("Hostname matches expected config")
 
 
-if hostname == "mtg":
+if hostname.strip() == "mtg":
     print("Testing to see if this hits just on lowercase not modified ....")
 
-if hostname == "MTG":
+if hostname.strip() == "MTG":
     print("Another test for all caps.......") 
 
 ## Always print out to the user
---
+++ ownLogic/simpleLogic2.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+print("We are giving away a trip.")
+print("need some info to determine what trip you are best suited for.")
+
+# putting functions on top
+def lastOption():
+    print("Since you can't answer simple questions:")
+    print("We are going to send you to Disney World in LA")
+    print("on the busiest day available.... Enjoy the tourist...")
+
+def noKidsAbroad():
+    while True:
+        seaSickKids = input("Does anybody in the family get sea sick? Y or N  ").strip().lower
+
+        if seaSickKids == "y":
+            print("You get a National Parks Pass and an RV rental")
+            break
+        elif seaSickKids == "n":
+            print("You just won a Carnaval Trip in the Bahamas")
+            break
+        else:
+            print("You don't know?.....")
+            lastOption()
+
+def takingKids():
+    while True:
+        kidsAbroad = input('Are you willing to travel abroad with the kids? Y or N  ').strip().lower()
+
+        if kidsAbroad == "n":
+            noKidsAbroad()
+            break
+
+        elif kidsAbroad == "y":
+            print("Your going to Europe to visit Castels")
+            break
+
+        else :
+            print("tough question and can't answer correctly, then")
+            lastOption()
+
+def abroad1():
+    while True:
+        drink = input("Do you like wine?  Y or N  ").strip().lower()
+
+        if drink == "y":
+            print("Sending you to Europe to go wine tasteing.  Enjoy")
+            break
+        elif drink =="n":
+            print("You're going to the Orient to discover new cultures")
+            break
+        else:
+            lastOption()
+        
+
+def travelFreedom():
+    while True:
+        abroadY = input("Do you want to travel overseas?  Y or N  ").strip().lower()
+
+        if abroadY == "y":
+            abroad1()
+            break
+        elif abroadY == "n":
+            print("Sending you to the Keys in Sunny Florida")
+            break
+        else :
+            lastOption()
+
+def takeKids(): 
+    while True:
+        kidsTake = input("Do you plan to take them with you? Enter Y or N  ").strip().lower()
+
+        if kidsTake == "y":
+            takingKids()
+            break
+
+        elif kidsTake == "n":
+            travelFreedom()
+            break
+
+        else:
+            print("So you can't answer simple questions. ")
+            lastOption()
+
+
+while True:
+    kids = input("Do you have children? Y or N  ").strip().lower()
+
+    if kids == "y":
+        takeKids()
+        break
+
+    elif kids =="n":
+        travelFreedom()
+        break
+
+    else: 
+        print("You don't know if you have kids?")
+        lastOption()
+
+
+# def kidsAbroad():
+ #   castels = input("Your going to Europe to visit Castels")
+
+print("Bon Voyage")
